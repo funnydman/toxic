b09ab3fbac36a79b0ccbf0c827cfc3bcedca81ec
funnydman <crazy-dzmitry@mail.ru>
2020-06-25 12:04:49 +0300

    implement file like endpoints

--- toxic/core/server/main.py
+++ toxic/core/server/main.py
@@ -1,5 +1,6 @@
 import selectors
-from socket import AF_INET, SOCK_STREAM, SOCK_NONBLOCK, socket
+from io import BytesIO
+from socket import AF_INET, SOCK_STREAM, SOCK_NONBLOCK, socket, SocketIO
 
 
 class TCPServer:
@@ -8,7 +9,7 @@ class TCPServer:
     option = SOCK_NONBLOCK
 
     def __init__(self) -> None:
-        self.socket = socket(family=self.address_family, type=self.type | self.option)
+        self.socket = socket(family=self.address_family, type=self.type)
 
         self.sel = selectors.DefaultSelector()
         self.sel.register(self.socket, selectors.EVENT_READ, self.accept)
@@ -18,11 +19,29 @@ class TCPServer:
 
     def accept(self, sock, mask):
         conn, addr = self.socket.accept()
+        self.wfile = conn.makefile('wb', buffering=-1)
+        self.rfile = conn.makefile('rb', buffering=-1)
+        # there something with read main loop in the SocketIO implementation
+        raw = SocketIO(self, 'rb')
+
+        buffer = BytesIO()
+        buffer.write(b'some')
+
+        # conn.recv_into(buffer)
+
+        # self.file = conn.makefile('rwb')
+        # self.file.write(b'some')
+        # self.file.read(1)
+
+        # print(self.rfile.read())
 
         self.sel.register(conn, selectors.EVENT_READ, self.read)
 
     def read(self, conn, mask):
-        data = conn.recv(1000)  # read as much as possible
+        # data = conn.recv(1000)  # read as much as possible
+        # self.wfile.write(b'test')
+        # data = self.rfile.read(1000)
+        data = None
         to_send = b"HTTP/1.1 200 OK\r\nContent-Type: application/json\r\n\r\n{\"name\":\"funnydman\"}\r\n"
         with conn:
             try:
@@ -30,6 +49,7 @@ class TCPServer:
                     conn.send(to_send)
             finally:
                 self.sel.unregister(conn)
+                self.close()
 
     def write(self):
         pass
@@ -39,7 +59,7 @@ class TCPServer:
         self.socket.listen()
 
         while True:
-            events = self.sel.select(timeout=0.2)
+            events = self.sel.select()
             for key, mask in events:
                 callback = key.data
                 callback(key.fileobj, mask)
@@ -55,4 +75,4 @@ class HTTPServer(TCPServer):
 if __name__ == '__main__':
     server = HTTPServer()
 
-    server.run(host='', port=8000)
+    server.run(host='', port=5000)
